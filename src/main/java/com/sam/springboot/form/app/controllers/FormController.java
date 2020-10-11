fb0e86cb3ef83201a6fdddfb3d3d6a48a515526d
chriscar <chriscar.sam@yahoo.com>
2020-10-11 13:13:32 -0500

    Mapeando los campos del formulario al objeto model

--- src/main/java/com/sam/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/sam/springboot/form/app/controllers/FormController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.sam.springboot.form.app.models.domain.Usuario;
 
 @Controller
@@ -23,16 +21,8 @@ public class FormController {
 	}
 	
 	@PostMapping("/form")
-	public String procesar(Model model,
-			@RequestParam(name="username") String username,
-			@RequestParam String password,
-			@RequestParam String email) {
-		
-		Usuario usuario = new Usuario();
-		usuario.setUsername(username);
-		usuario.setEmail(email);
-		usuario.setPassword(password);
-		
+	public String procesar(Usuario usuario, Model model) {
+				
 		model.addAttribute("titulo", "Resultado form");
 		model.addAttribute("usuario", usuario);
 		
