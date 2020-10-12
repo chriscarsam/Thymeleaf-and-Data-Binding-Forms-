8ada07c8ecb0f9b279658f093ee15f5ecb8dc729
chriscar <chriscar.sam@yahoo.com>
2020-10-12 11:31:35 -0500

    Añadiendo los mensajes de errores en la vista del formulario

--- src/main/java/com/sam/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/sam/springboot/form/app/controllers/FormController.java
@@ -22,7 +22,9 @@ public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
+		Usuario usuario = new Usuario();
 		model.addAttribute("titulo", "Formulario usuarios");
+		model.addAttribute("usuario", usuario);
 		return "form";
 	}
 	
