d1ff0d724f507f9eff546ba9fff8008847070072
chriscar <chriscar.sam@yahoo.com>
2020-10-13 10:14:00 -0500

    La anotación @SessionAttributes para mantner los datos durante el ciclo del form

--- src/main/java/com/sam/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/sam/springboot/form/app/controllers/FormController.java
@@ -7,9 +7,13 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
 import com.sam.springboot.form.app.models.domain.Usuario;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 	
 	@GetMapping("/")
@@ -29,7 +33,7 @@ public class FormController {
 	}
 	
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
 								
 		model.addAttribute("titulo", "Resultado form");
 		
@@ -38,7 +42,7 @@ public class FormController {
 			return "form";
 		}
 		model.addAttribute("usuario", usuario);
-		
+		status.setComplete();
 		return "resultado";
 	}
 }
