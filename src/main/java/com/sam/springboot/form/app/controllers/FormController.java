fd3edde7d6f116f8f101bfa6bb4ce436e0eecbbf
chriscar <chriscar.sam@yahoo.com>
2020-10-14 10:40:02 -0500

    Validación personalizada usando una clase Validation

--- src/main/java/com/sam/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/sam/springboot/form/app/controllers/FormController.java
@@ -2,6 +2,7 @@ package com.sam.springboot.form.app.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,10 +12,14 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import com.sam.springboot.form.app.models.domain.Usuario;
+import com.sam.springboot.form.app.validation.UsuarioValidador;
 
 @Controller
 @SessionAttributes("usuario")
 public class FormController {
+	
+	@Autowired
+	private UsuarioValidador validador;
 	
 	@GetMapping("/")
 	public String home() {
@@ -34,7 +39,9 @@ public class FormController {
 	
 	@PostMapping("/form")
 	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
-								
+							
+		validador.validate(usuario, result);
+		
 		model.addAttribute("titulo", "Resultado form");
 		
 		if(result.hasErrors()) {
