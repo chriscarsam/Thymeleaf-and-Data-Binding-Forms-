d44540dc8864b99bcd648f6a3ae75b3e1cc2ec4f
chriscar <chriscar.sam@yahoo.com>
2020-10-17 09:40:42 -0500

    Lista select desplegable

--- src/main/java/com/sam/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/sam/springboot/form/app/controllers/FormController.java
@@ -1,7 +1,9 @@
 package com.sam.springboot.form.app.controllers;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -13,6 +15,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -42,6 +45,11 @@ public class FormController {
 	@GetMapping("/")
 	public String home() {
 		return "forward:/form";
+	}
+	
+	@ModelAttribute("paises")
+	public List<String> paises(){
+		return Arrays.asList("España", "Ecuador", "México", "Chile", "Argentina", "Perú", "Colombia", "Venezuela");
 	}
 
 	@GetMapping("/form")
